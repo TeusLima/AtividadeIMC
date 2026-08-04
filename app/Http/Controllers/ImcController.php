d291e5da16c1fca3ef39c9f3480b358daa5a7799
Mateus Lima Santos <mateus58868146@aluno.ba.senac.br>
2026-08-03 21:00:15 -0300

    Atividade Laravel IMC

---
+++ app/Http/Controllers/ImcController.php
@@ -0,0 +1,62 @@
+<?php
+
+require "../app/Models/ImcModel.php";
+
+class ImcController
+{
+    public function CalculoIMC($peso, $altura)
+    {
+        if (isset($peso) && isset($altura)) {
+
+            $resultado = [];
+            // Verifica se os valores de peso e altura são válidos (não são zero)
+            if ($peso > 0 && $altura > 0) {
+                // Calcula o IMC
+                $imc = $peso / ($altura ** 2);
+
+                // Arredonda o resultado para duas casas decimais
+                $resultado["imc"] = round($imc, 2);
+                // Utiliza uma estrutura de switch para determinar a categoria do IMC
+                switch (true) {
+                    case ($imc < 18.5):
+                        $resultado["faixa"] = "Abaixo do peso";
+                        break;
+                    case ($imc >= 18.5 && $imc < 25):
+                        $resultado["faixa"] = "Peso normal";
+                        break;
+                    case ($imc >= 25 && $imc < 30):
+                        $resultado["faixa"] = "Sobrepeso";
+                        break;
+                    default:
+                        $resultado["faixa"] = "Obesidade";
+                }
+            } else {
+                $resultado["faixa"] = "O peso e a altura devem ser valores positivos.";
+            }
+        } else {
+            $resultado["faixa"] = "Por favor, insira seu peso e altura.";
+        }
+
+        return $resultado;
+
+    }
+
+    public function enviarDados($peso, $altura){
+
+        $model = new ModelImc();
+        $resultadoModel = $model->processarDados($peso, $altura);
+
+        return $resultadoModel;
+
+    }
+
+    public function pegarDados(){
+
+        $model = new ModelImc();
+        return $model->selecionarDados();
+
+    }
+
+}
+
+?>
---
+++ resources/views/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <form method="post">
+            <div class="mb-3">
+                <label for="formGroupExampleInput" class="form-label">PESO</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="peso" name="peso">
+            </div>
+            <div class="mb-3">
+                <label for="formGroupExampleInput2" class="form-label">ALTURA</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="altura" name="altura">
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Calcular</button>
+            </div>
+
+            <br><br><br>
+        </form>
+
+
+
+    </div>
+</body>
+
+</html>
