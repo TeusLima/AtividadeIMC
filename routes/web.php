2d757df5448b84ed9adc646b26b06f9b6568bcfb
Mateus Lima Santos <mateus58868146@aluno.ba.senac.br>
2026-08-18 20:43:06 -0300

    Logica dos niveis de acesso

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,11 @@ use App\Http\Middleware\Autentication;
 
 Route::middleware(Autentication::class)->group(function (){
     
-    
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('imc.dash');
     Route::post('/salvar', [ImcController::class, 'store'])->name('imc.salvar');
     Route::put('/dashboard/update/{id}', [DashboardController::class, 'update'])->name('dash.update');
     Route::delete('/dashboard/delete/{id}', [DashboardController::class, 'destroy'])->name('dash.delete');
+    
 });
     
 Route::get('/', [ImcController::class, 'index'])->name('imc.index');
